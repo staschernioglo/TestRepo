a6ed5c6b1bf2200447e869cea2dc3a18d3b2be48
Admin <Admin@DESKTOP-VGLN2P9>
2022-02-09 22:52:59 +0200

    Add new line 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Master branch");
+            Console.WriteLine("New line 1");
         }
     }
 }
